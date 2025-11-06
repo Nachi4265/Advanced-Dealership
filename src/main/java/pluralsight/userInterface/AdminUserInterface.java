1e5742c64c81d356a14b9353dd5d45a1c46d141c
Nachi4265 <hvirgen@my.yearupunited.org>
2025-11-06 10:01:01 -0500

    Added Admin password

--- src/main/java/pluralsight/userInterface/AdminUserInterface.java
+++ src/main/java/pluralsight/userInterface/AdminUserInterface.java
@@ -2,7 +2,39 @@ package pluralsight.userInterface;
 
 public class AdminUserInterface {
 
-    public void enterPassword(){
+    public void adminDisplay() {
+
+        if(enterPassword()){
+
+            while (true){
+                System.out.println("========| Admin Menu |========");
+                System.out.println("Please choose and option");
+                System.out.println("1) List all Contracts");
+                System.out.println("2) List Last 10 Contracts");
+                System.out.println("3) Return to Dealership Menu");
+
+                int command = InputCollector.promptForInt("Enter command");
+
+                switch (command){
+                    case 1:
+                        listAllContracts();
+                        break;
+                    case 2:
+                        listLast10Contracts();
+                        break;
+                    case 3:
+                        return;
+                    default:
+                        System.out.println("Invalid command!");
+                }
+            }
+        }
+
+        //Return to Dealership UI
+
+    }
+
+    public boolean enterPassword(){
         System.out.println("Welcome to Dealership Admin terminal");
         System.out.println("------------------------------------");
 
@@ -16,56 +48,22 @@ public class AdminUserInterface {
 
             if(passwordEntered.equalsIgnoreCase(correctPassword)){
                 authenticated = true;
-                adminDisplay();
+//                adminDisplay();
 
             }else if(attempts>1){
                 attempts -= 1 ;
                 System.out.println("Incorrect password Access denied");
                 System.out.println("You have " + attempts + " attempts left");
-            }else {
+            }else{
                 System.out.println("YOU ARE OUT OF ATTEMPTS RETURNING TO DEALERSHIP!");
-                return;
+                return false;
             }
         }
+        return authenticated;
     }
-
-
-
-
-    public void adminDisplay() {
-
-        enterPassword();
-
-
-        if(enterPassword() == true)
-        while (true){
-            System.out.println("=======| Admin Menu |========");
-            System.out.println("Please choose and option");
-            System.out.println("1) List all Contracts");
-            System.out.println("2) List Last 10 Contracts");
-            System.out.println("3) Return to Dealership Menu");
-
-            int command = InputCollector.promptForInt("Enter command");
-
-            switch (command){
-                case 1:
-                    listAllContracts();
-                    break;
-                case 2:
-                    listLast10Contracts();
-                    break;
-                case 3:
-                    return;
-                default:
-                    System.out.println("Invalid command!");
-
-            }
-        }
-
-    }
-
 
     private void listLast10Contracts() {
+
     }
 
     private void listAllContracts() {
